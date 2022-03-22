151a0d903ff90f97c2cbe635cc76cd251d0ecddb
Mi Do <96797434+maskros-m@users.noreply.github.com>
2022-03-22 08:37:27 +0100

    Add files via upload

---
+++ Lab513_2DArray_Chessboard.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	char chessboard[8][8];
+	int row, square;
+	/* Rooks */
+	for (row = 0; row < 8; row += 7) {
+	    for (square = 0; square < 8; square++) {
+	        chessboard[row][square] = chessboard[row][square+7] = 'R';
+	        chessboard[row][++square] = chessboard[row][square+5] = 'N';
+	        chessboard[row][++square] = chessboard[row][square+3] = 'B';
+            chessboard[row][++square] = 'Q';
+            chessboard[row][++square] = 'K';
+	    }
+	}
+	
+	for (row = 1; row < 8; row += 5) {
+	    for (square = 0; square < 8; square++)
+	        chessboard[row][square] = 'P';
+	}
+	
+	for (row = 2; row < 6; row++) {
+	    for (square = 0; square < 8; square++) {
+	        chessboard[row][square] = ' ';
+	    }
+	}
+	
+	for (row=0; row < 8; row++) {
+	    for (square=0; square<8; square++) {
+	        printf("%c", chessboard[row][square]);
+	    }
+	    printf("\n");
+	}
+	    
+	return 0;
+}
