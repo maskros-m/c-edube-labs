3d5595bdab79e015abd6037f3c872c0c9d56d30d
Mi Do <96797434+maskros-m@users.noreply.github.com>
2022-03-25 09:28:44 +0100

    Update Lab-514-MemoryOnDemand.c

--- Lab-514-MemoryOnDemand.c
+++ Lab-514-MemoryOnDemand.c
@@ -1,4 +1,44 @@
-﻿#include <stdio.h>
+/*
+Write a program that allocates memory of a size requested by the user.
+This program requests a number from the user and checks if that number is less than 1 MB (1024*1024). If it's not, then the program prints the message: "Too much memory requested.".
+
+In the next step, the program allocates a one-dimesional array of characters (char) and fills this array with characters from "A" to "Z" - the first element (index 0) contains "A", the 26th element (index 25) contains "Z", the 27th element (index 26) contains "A" and so on.
+
+Then, the program prints the first 400 bytes of an array (or the whole array if it's smaller than 400 characters), 40 characters in each row.
+To simplify the program, you can use the break or continue statements. Remember to free up the array memory at the end of the program.
+
+Your version of the program must print the same result as the expected output.
+Sample input
+100
+
+Expected output
+ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMN
+OPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZAB
+CDEFGHIJKLMNOPQRSTUV
+
+Sample input
+500
+
+Expected output
+ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMN
+OPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZAB
+CDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOP
+QRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCD
+EFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQR
+STUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEF
+GHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRST
+UVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGH
+IJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUV
+WXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJ
+
+Sample input
+1200500
+
+Expected output
+Too much memory requested.
+*/
+
+#include <stdio.h>
 #include <stdlib.h>
 
 int main()
