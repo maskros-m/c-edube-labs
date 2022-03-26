da521eddb39517ab5b296b98d423f6254e4e1e46
Mi Do <96797434+maskros-m@users.noreply.github.com>
2022-03-26 16:53:22 +0100

    Update Lab-516_Arrays-of-pointers-Matrices.c

--- Lab-516_Arrays-of-pointers-Matrices.c
+++ Lab-516_Arrays-of-pointers-Matrices.c
@@ -34,33 +34,38 @@ int main() {
   
   /* Takes user input and allocate memory to matrix */
   scanf("%d", &tabsize);
-  multab = (int **)malloc(tabsize * sizeof(int *));
-  for (int row = 0; row < tabsize; row++) {
-    multab[row] = (int *) malloc(tabsize * sizeof(int));
+  
+  /* Checking matrix's size */
+  if (tabsize > 20) puts("Matrix too big.");
+  else {
+       multab = (int **)malloc(tabsize * sizeof(int *));
+       for (int row = 0; row < tabsize; row++) {
+         multab[row] = (int *) malloc(tabsize * sizeof(int));
+       }
+
+       /* Assigning values to each cell of matrix */
+       for (r = 0; r < tabsize; r++) {
+         for (c = 0; c < tabsize; c++) {
+           multab[r][c] = (r + 1) * (c + 1);
+         }
+       }
+
+       /* Printing matrix */
+       for (r = 0; r < tabsize + 1; r++) 
+         printf("%*.*d", 4, 0, r);
+
+       for (r = 0; r < tabsize; r++) {
+         printf("\n");
+         printf("%*.*d", 4, 0, r+1);
+         for (c = 0; c < tabsize; c++) {
+           printf("%*.%d", 4, 0, multab[r][c]);
+         }
+       }
+
+       /* Deallocating memory */
+       for (r = 0; r < tabsize; r++) free(multab[r]);
+       free(multab);
   }
-  
-  /* Assigning values to each cell of matrix */
-  for (r = 0; r < tabsize; r++) {
-    for (c = 0; c < tabsize; c++) {
-      multab[r][c] = (r + 1) * (c + 1);
-    }
-  }
-  
-  /* Printing matrix */
-  for (r = 0; r < tabsize + 1; r++) 
-    printf("%*.*d", 4, 0, r);
-  
-  for (r = 0; r < tabsize; r++) {
-    printf("\n");
-    printf("%*.*d", 4, 0, r+1);
-    for (c = 0; c < tabsize; c++) {
-      printf("%*.%d", 4, 0, multab[r][c]);
-    }
-  }
-  
-  /* Deallocating memory */
-  for (r = 0; r < tabsize; r++) free(multab[r]);
-  free(multab);
   
   return 0;
 }
