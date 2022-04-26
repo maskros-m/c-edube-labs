244c712a70ce37657ec9e0d51fe94b9b80bad22a
Mi Do <96797434+maskros-m@users.noreply.github.com>
2022-04-26 23:52:49 +0200

    Add files via upload

---
+++ Lab-412-Fibbonacci.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	/* your code */
+	int fib[10], i;
+	
+	fib[0] = fib[1] = 1;
+	
+	for (i = 2; i < 10; i++) {
+	    fib[i] = fib[i-2] + fib[i-1];
+	}
+	
+	for (i = 0; i < 10; i++) {
+	    printf("%d ", fib[i]);
+	}
+	
+	printf("\n");
+	for (i = 0; i < 10; i+=2) {
+	    printf("%d ", fib[i]);
+	}
+	
+	printf("\n");
+	for (i = 1; i < 10; i+=2) {
+	    printf("%d ", fib[i]);
+	}
+	return 0;
+}
